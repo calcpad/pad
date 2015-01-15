0114669532ee99aa5b330f02f1edb5edc7db37d8
benstr <ben@benstrahan.com>
2015-01-15 12:42:20 -0600

    Added delete functionality

--- client/views/pads/_padItem.js
+++ client/views/pads/_padItem.js
@@ -1,9 +1,11 @@
 Template._padItem.helpers({
   routeQuery: function(){
-    console.log(this);
+    //console.log(this);
     return "padId="+this._id
   }
 });
+// FYI: padListToggle is a global helper in client/views/helpers.js
+
 Template._padItem.events({  });
 Template._padItem.rendered = function() {  };
 Template._padItem.created = function() {  };
--- client/views/pads/padsList.js
+++ client/views/pads/padsList.js
@@ -1,7 +1,19 @@
-Template.padsList.helpers({  });
+Template.padsList.helpers({ });
+// FYI: padListToggle is a global helper in client/views/helpers.js
+
 Template.padsList.events({  });
+Template.appLayout.events({
+  "click [data-action=padListToggleEdit]": function() {
+    Session.set('padListEdit',true);
+  },
+  "click [data-action=padListToggleIndex]": function() {
+    Session.set('padListEdit',false);
+  }
+});
+
+
 Template.padsList.rendered = function() {
-  console.log(this);
+  //console.log(this);
 };
 Template.padsList.created = function() {  };
 Template.padsList.destroyed = function() {  };
